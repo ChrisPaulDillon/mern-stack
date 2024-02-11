bddc3f6d257d3983d56c136d3f1f09f103eec48c
Chris <chrispauldillon@live.com>
2024-02-11 15:39:48 +0000

    Added delete, update workout controller

--- server/src/controllers/workoutController.ts
+++ server/src/controllers/workoutController.ts
@@ -37,8 +37,39 @@ export const createWorkout = async (req, res) => {
   }
 };
 
-// delete a workout
-export const deleteWorkout = async (req, res) => {};
+export const deleteWorkout = async (req, res) => {
+  const { id } = req.params;
 
-// update a workout
-export const updateWorkout = async (req, res) => {};
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "No such workout" });
+  }
+
+  const workout = await workoutModel.findOneAndDelete({ _id: id });
+
+  if (!workout) {
+    return res.status(400).json({ error: "No such workout" });
+  }
+
+  res.status(200).json(workout);
+};
+
+export const updateWorkout = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "No such workout" });
+  }
+
+  const workout = await workoutModel.findOneAndUpdate(
+    { _id: id },
+    {
+      ...req.body,
+    }
+  );
+
+  if (!workout) {
+    return res.status(400).json({ error: "No such workout" });
+  }
+
+  res.status(200).json(workout);
+};
